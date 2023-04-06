2cef886d12506a0b458b0423bd71485affd0bd2b
Anirban Mishra <anirbanmishra7005@gmail.com>
2023-04-06 11:45:18 +0530

    warning update

--- src/layout/components/helpers/Warning.js
+++ src/layout/components/helpers/Warning.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Warning = () => {
+  const [visible, setVisibility] = useState(true);
   return (
     <>
-      <div
-        className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
-        role="alert"
-      >
-        <p className="font-bold">In Progress</p>
-        <p>This site is not ready..! working in progress.</p>
-      </div>
+      {visible && (
+        <div
+          className="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 flex gap-10"
+          role="alert"
+        >
+          <button
+            onClick={() => {
+              setVisibility(false);
+            }}
+            className="text-3xl"
+          >
+            X
+          </button>
+          <div>
+            <p className="font-bold">In Progress</p>
+            <p>This site is not ready..! working in progress.</p>
+          </div>
+        </div>
+      )}
     </>
   );
 };
